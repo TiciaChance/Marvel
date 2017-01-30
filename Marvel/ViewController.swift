bb75e9dd24b7f8bcbfe11b3d6b27565356feff8d
Ticia Chance <laticia.chance@gmail.com>
2017-01-30 12:33:39 -0500

    Initial Commit

---
+++ Marvel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Ticia Chance on 1/30/17.
+//  Copyright © 2017 Ticia Chance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
